39a560314ab0bb5dce0c9493db6f2f9fce906f71
OriaSadai <oriasadai@gmail.com>
2023-09-16 14:32:52 +0300

    first commit

---
+++ src/main/java/pollProject/pollService/PollServiceApplication.java
@@ -0,0 +1,13 @@
+package pollProject.pollService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pollProject/pollService/PollServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pollProject.pollService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
